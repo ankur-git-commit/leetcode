4ec57e45d914a5f3b1d48553a70555889aea9c60
Ankur <ankur.kumar.dev@outlook.com>
2025-08-06 01:21:35 +0530

    Time: 13 ms (13.92%) | Memory: 24.2 MB (20.05%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -8,48 +8,50 @@ class Solution:
 
         for num, count in freq_map.items():
             freq[count].append(num)
-
+        
         res = []
-        for i in range(len(freq) - 1, -1):
+        for i in range(len(freq) - 1, -1, -1):
+            # print(i)
             for num in freq[i]:
+                # print(res)
                 res.append(num)
                 if len(res) == k:
                     return res
 
 
-        # All solutions below are O(nlogn) time complexity cause of sorting method that is being used
-        # Solution 1
-        # with counter dictionary and list comprehension
-        elements_freq = Counter(nums)
-        sort_by_values = sorted(
-            elements_freq.items(), key=lambda item: item[1], reverse=True
-        )
-        return [sort_by_values[i][0] for i in range(k)]
+        # # All solutions below are O(nlogn) time complexity cause of sorting method
+        # # Solution 1
+        # # with counter dictionary and list comprehension
+        # elements_freq = Counter(nums)
+        # sort_by_values = sorted(
+        #     elements_freq.items(), key=lambda item: item[1], reverse=True
+        # )
+        # return [sort_by_values[i][0] for i in range(k)]
 
-        # Solution 2
-        # without counter dictionary and list comprehension
-        ele_freq = {}
-        for i in range(len(nums)):
-            ele_freq[nums[i]] = 1 + ele_freq.get(nums[i], 0)
+        # # Solution 2
+        # # without counter dictionary and list comprehension
+        # ele_freq = {}
+        # for i in range(len(nums)):
+        #     ele_freq[nums[i]] = 1 + ele_freq.get(nums[i], 0)
 
-        sort_by_value = sorted(ele_freq.items(), key=lambda item: item[1], reverse=True)
+        # sort_by_value = sorted(ele_freq.items(), key=lambda item: item[1], reverse=True)
 
-        result = []
-        for i in range(k):
-            result.append(sort_by_value[i][0])
+        # result = []
+        # for i in range(k):
+        #     result.append(sort_by_value[i][0])
 
-        return result
+        # return result
 
-        # Solution 3
-        ele_freq = {}
-        for i in range(len(nums)):
-            ele_freq[nums[i]] = 1 + ele_freq.get(nums[i], 0)
-        arr = []
-        for element, count in ele_freq.items():
-            arr.append([count, element])
-        arr.sort()  # can also do arr.sort(reverse=True)
-        print(arr)
-        res = []
-        while len(res) < k:
-            res.append(arr.pop()[1])
-        return res
+        # # Solution 3
+        # ele_freq = {}
+        # for i in range(len(nums)):
+        #     ele_freq[nums[i]] = 1 + ele_freq.get(nums[i], 0)
+        # arr = []
+        # for element, count in ele_freq.items():
+        #     arr.append([count, element])
+        # arr.sort()  # can also do arr.sort(reverse=True)
+        # print(arr)
+        # res = []
+        # while len(res) < k:
+        #     res.append(arr.pop()[1])
+        # return res
