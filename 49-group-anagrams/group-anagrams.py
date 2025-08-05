fcb4ed9903d3732963827e936833d226de7e7c0e
Ankur <ankur.kumar.dev@outlook.com>
2025-08-05 23:17:30 +0530

    Time: 9 ms (89.94%) | Memory: 20.6 MB (89.90%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+
+        for item in strs:
+            current_item = "".join(sorted(item))
+
+            if current_item not in anagrams:
+                anagrams[current_item] = [item]
+            else:
+                anagrams[current_item].append(item)
+
+        result = []
+        for key, value in anagrams.items():
+            result.append(value)
+
+        return result
