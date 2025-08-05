09928bcf1920dbe1292d317d4f857daf60d4c3b5
Ankur <ankur.kumar.dev@outlook.com>
2025-08-05 23:57:54 +0530

    Time: 10 ms (26.42%) | Memory: 21.3 MB (61.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+
         # Solution 1
         # with counter dictionary and list comprehension
-        elements_freq = Counter(nums)
-        sort_by_values = sorted(elements_freq.items(), key=lambda item: item[1], reverse=True)
-        return [sort_by_values[i][0] for i in range(k)]
-        
-        # Solution 2
-        # without counter dictionary and list comprehension
+        # elements_freq = Counter(nums)
+        # sort_by_values = sorted(elements_freq.items(), key=lambda item: item[1], reverse=True)
+        # return [sort_by_values[i][0] for i in range(k)]
+
+        # # Solution 2
+        # # without counter dictionary and list comprehension
+        # ele_freq = {}
+        # for i in range(len(nums)):
+        #     ele_freq[nums[i]] = 1 + ele_freq.get(nums[i], 0)
+
+        # sort_by_value = sorted(ele_freq.items(), key=lambda item: item[1], reverse=True)
+
+        # result = []
+        # for i in range(k):
+        #     result.append(sort_by_value[i][0])
+
+        # return result
+
+        # Solution 3
         ele_freq = {}
         for i in range(len(nums)):
             ele_freq[nums[i]] = 1 + ele_freq.get(nums[i], 0)
-
-        sort_by_value = sorted(ele_freq.items(), key=lambda item: item[1], reverse=True)
-
-        result = []
-        for i in range(k):
-            result.append(sort_by_value[i][0])
-
-        return result
+        arr = []
+        for element, count in ele_freq.items():
+            arr.append([count, element])
+        arr.sort()  # can also do arr.sort(reverse=True)
+        print(arr)
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        return res
