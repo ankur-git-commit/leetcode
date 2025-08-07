d51e184685dd99cd8789758dfdc728521789967a
Ankur <ankur.kumar.dev@outlook.com>
2025-08-07 19:42:48 +0530

    Time: 3241 ms (5.75%) | Memory: 18.2 MB (19.28%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,14 +2,16 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
         # Works but takes too much time O(n) Solution
-        hash_map = {"".join(sorted(s1)): 1}
+        # hash_map = {"".join(sorted(s1)): 1}
+        sorted_s1 = "".join(sorted(s1))
         length_s1 = len(s1)
 
         for i in range(0, len(s2)):
             if i + length_s1 > len(s2):
                 break
             per = "".join(sorted(s2[i : i + length_s1]))
-            if per in hash_map:
+            # if per in hash_map:
+            if per == sorted_s1:
                 return True
 
         return False
