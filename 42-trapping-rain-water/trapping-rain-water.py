68b5db090de8f49ef9b687a9201387559b9f6e3e
Ankur <ankur.kumar.dev@outlook.com>
2025-08-19 03:20:46 +0530

    Time: 23 ms (33.27%) | Memory: 19.8 MB (5.25%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,19 +3,23 @@ class Solution:
         # initial thought - don't know how to start the algorithm or even loop through it
         # obviously the answer lies between the walls, if the height is 0 or lower value compared to the height of the enclosing walls then that is the amount of water that is stored.
         # formula =  min(L, R) - height[i] 
+
+        # solution 1
+        # Time complexity: O(n)
+        # Space complexity: O(n) - creating 2 extra arrays to store the max_left and max_right height for each element
         L, R = 0, len(height) - 1
-        left_array, right_array = [0], [0]
+        n = len(height)
+        left_array, right_array = [0] * n, [0] * n
         max_left_val, max_right_val = 0, 0
 
-        for i in range(1, len(height)):
+        for i in range(1, n):
             max_left_val = max(max_left_val, height[i - 1])
-            left_array.append(max_left_val)
+            left_array[i] = (max_left_val)
         
-        for i in range(len(height)-2, -1, -1):
+        for i in range(n-2, -1, -1):
             max_right_val = max(max_right_val, height[i + 1])
-            right_array.append(max_right_val)
+            right_array[i] = (max_right_val)
         
-        right_array.reverse()
 
         res = 0
         print(left_array, right_array)
