e5c493efb3d9fea17effd7c57c47d0b77c189597
Ankur <ankur.kumar.dev@outlook.com>
2025-08-19 04:02:56 +0530

    Time: 13 ms (55.15%) | Memory: 19.5 MB (11.45%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,6 +4,31 @@ class Solution:
         # obviously the answer lies between the walls, if the height is 0 or lower value compared to the height of the enclosing walls then that is the amount of water that is stored.
         # formula =  min(L, R) - height[i]
 
+        # solution 1
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        n = len(height)
+        if n <= 2 or not n:
+            return 0
+        
+        L, R = 0, n - 1
+        max_left, max_right = height[L], height[R]
+        res = 0
+        while L < R:
+            if max_left < max_right:
+                L += 1
+                max_left = max(max_left, height[L])
+                res += max_left - height[L]
+            else:
+                R -= 1
+                max_right = max(max_right, height[R])
+                res += max_right - height[R]
+
+        return res
+
+
+
+
         # solution 2
         # Time complexity: O(n)
         # Space complexity: O(n) - creating 2 extra arrays to store the max_left and max_right height for each element
@@ -14,12 +39,11 @@ class Solution:
         left_max = [0] * n
         right_max = [0] * n
         lm, rm = 0, 0
-
         for i in range(n):
             left_max[i] = lm
             lm = max(lm, height[i])
-
             j = n - 1 - i
+            print(j)
             right_max[j] = rm
             rm = max(rm, height[j])
         
@@ -29,25 +53,25 @@ class Solution:
         
         return res
 
-        # solution 1 (bad solution)
+        # solution 1 (bad solution - creating multiple loops )
         # Time complexity: O(n)
-        # Space complexity: O(n) - creating 2 extra arrays to store the max_left and max_right height for each element
-        # L, R = 0, len(height) - 1
-        # n = len(height)
-        # left_array, right_array = [0] * n, [0] * n
-        # max_left_val, max_right_val = 0, 0
+        # Space complexity: O(n) - creating 2 extra arrays to store the max_left and max_right height for each element 
+        L, R = 0, len(height) - 1
+        n = len(height)
+        left_array, right_array = [0] * n, [0] * n
+        max_left_val, max_right_val = 0, 0
 
-        # for i in range(1, n):
-        #     max_left_val = max(max_left_val, height[i - 1])
-        #     left_array[i] = max_left_val
+        for i in range(1, n):
+            max_left_val = max(max_left_val, height[i - 1])
+            left_array[i] = max_left_val
 
-        # for i in range(n - 2, -1, -1):
-        #     max_right_val = max(max_right_val, height[i + 1])
-        #     right_array[i] = max_right_val
+        for i in range(n - 2, -1, -1):
+            max_right_val = max(max_right_val, height[i + 1])
+            right_array[i] = max_right_val
 
-        # res = 0
-        # for i in range(len(height)):
-        #     curr_val = min(left_array[i], right_array[i]) - height[i]
-        #     res += curr_val if curr_val > 0 else 0
+        res = 0
+        for i in range(len(height)):
+            curr_val = min(left_array[i], right_array[i]) - height[i]
+            res += curr_val if curr_val > 0 else 0
 
-        # return res
+        return res
