f2ee3ce60d7159f1999ca782c9b9d78bcab0339b
Ankur <ankur.kumar.dev@outlook.com>
2025-08-19 03:13:17 +0530

    Time: 37 ms (8.25%) | Memory: 20 MB (5.25%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,27 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # initial thought - don't know how to start the algorithm or even loop through it
+        # obviously the answer lies between the walls, if the height is 0 or lower value compared to the height of the enclosing walls then that is the amount of water that is stored.
+        # formula =  min(L, R) - height[i] 
+        L, R = 0, len(height) - 1
+        left_array, right_array = [0], [0]
+        max_left_val, max_right_val = 0, 0
+
+        for i in range(1, len(height)):
+            max_left_val = max(max_left_val, height[i - 1])
+            left_array.append(max_left_val)
+        
+        for i in range(len(height)-2, -1, -1):
+            max_right_val = max(max_right_val, height[i + 1])
+            right_array.append(max_right_val)
+        
+        right_array.reverse()
+
+        res = 0
+        print(left_array, right_array)
+
+        for i in range(len(height)):
+            curr_val = min(left_array[i], right_array[i]) - height[i] 
+            res += curr_val if curr_val > 0 else 0
+        
+        return res
