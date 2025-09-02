60f3c6c7bb83336c74318fee13b1305ccdcd2477
Ankur <ankur.kumar.dev@outlook.com>
2025-09-02 17:32:34 +0530

    Time: 239 ms (22.26%) | Memory: 40.5 MB (25.12%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,5 +1,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        if len(position) <= 1:
+            return 1
+
         pair = [[p, s] for p,s in zip(position, speed)]
 
         stack = []
