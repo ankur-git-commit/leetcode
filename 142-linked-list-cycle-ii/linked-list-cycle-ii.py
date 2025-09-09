00843e9413666ade6c5a7ba514cb923fa86835e4
Ankur <ankur.kumar.dev@outlook.com>
2025-09-09 15:53:40 +0530

    Time: 35 ms (97.41%) | Memory: 19.7 MB (53.03%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,16 +6,32 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        hash_map = {}
-        pos = 0
-        curr = head
-
-        while curr:
-            if curr in hash_map:
-                # We’ve seen this node before → cycle entry
-                return curr
-            hash_map[curr] = pos
-            pos += 1
-            curr = curr.next
-
+        # without O(n) memory space using two-pointer(slow and fast)
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
         return None
+
+
+        # using hash_map
+        # hash_map = {}
+        # pos = 0
+        # curr = head
+
+        # while curr:
+        #     if curr in hash_map:
+        #         # We’ve seen this node before → cycle entry
+        #         return curr
+        #     hash_map[curr] = pos
+        #     pos += 1
+        #     curr = curr.next
+
+        # return None
