31e92c64a8eb37fbc3574b47d3f71730b3a13b7e
Ankur <ankur.kumar.dev@outlook.com>
2025-09-13 19:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (14.33%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,12 +5,31 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, temp = None, None
-        cur = head
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
+        # Recursive solution
+        # time - O(n)
+        # space - O(n)
+        return self.recursive_revList(head)
+
+    def recursive_revList(self, cur, prev=None):
+        if not cur:
+            return prev
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+        return self.recursive_revList(cur, prev)
+
+
+        # Iterative solution 
+        # time - O(n)
+        # space - O(1)
+
+        # prev = None
+        # cur = head
+        # while cur:
+        #     temp = cur.next
+        #     cur.next = prev
+        #     prev = cur
+        #     cur = temp
         
-        return prev
+        # return prev
