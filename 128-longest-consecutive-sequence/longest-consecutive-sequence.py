4b52fecb7c561fc2bbbec792aed2b614200aab67
Ankur <ankur.kumar.dev@outlook.com>
2025-09-17 01:37:26 +0530

    Time: 47 ms (74.63%) | Memory: 33.2 MB (80.78%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -7,10 +7,13 @@ class Solution:
         freq_count = set(nums)
 
         for i in freq_count:
-            if i - 1 not in freq_count:
-                count = 0
+            # count = 0
+            if i-1 not in freq_count:
+                # current_element = i
+                count = 1 
                 while i + count in freq_count:
                     count += 1
+                    # current_element += 1
                 res = max(res, count)
         
         return res
