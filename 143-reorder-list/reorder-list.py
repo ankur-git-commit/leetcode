7e2e18ec7d3b91548fbeaf3dd55298ead78d3883
Ankur <ankur.kumar.dev@outlook.com>
2025-09-17 01:17:28 +0530

    Time: 3 ms (45.63%) | Memory: 23.3 MB (38.35%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,16 +8,15 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # divide the list into two parts
-        slow, fast = head, head.next
+        # first we split/divide the list using slow and fast pointers
+        slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        # for the last element of the 1st half, set its next pointer to Null
+        
         second = slow.next
         slow.next = None
-
-        # reverse the 2nd half
+        # now we reverse the second part of the list
         prev = None
         while second:
             temp = second.next
@@ -25,14 +24,15 @@ class Solution:
             prev = second
             second = temp
         
-        # merge the the parts
-        first, second = head, prev  # prev is pointing to the last element of the 2nd part of the list
+        # now we merge the 2 lists
+        first, second = head, prev
         while second:
             temp1 = first.next
             temp2 = second.next
             first.next = second
             second.next = temp1
             first, second = temp1, temp2
+
 
 
 
