0b613ca472e80715e1abccb4ed6f98f64cd929d5
Ankur <ankur.kumar.dev@outlook.com>
2025-09-18 18:18:58 +0530

    Time: 168 ms (34.84%) | Memory: 19 MB (59.12%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,39 +1,18 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # n = number of piles of bananas
-        # i-th pile which is the index has piles[i] number of bananas
-        # h = hours
-        # k = speed of eating banaas per hour
-
-        
         L, R = 1, max(piles)
-
-        while L<=R:
-            k = (L + R) // 2 
-            total_time = 0
+        min_speed = 0
+        while L <= R:
+            k = (L+R)// 2
             
-            for p in piles:
-                total_time += math.ceil(p / k)
-                
-            
-            if total_time <= h:
-                res = k
-                R =  k - 1
+            total = 0
+            for pile in piles:
+                total += math.ceil(pile / k)
+                if total > h:
+                    break
+            if total <= h:
+                min_speed = k
+                R = k - 1
             else:
                 L = k + 1
-
-        return res
-
-
-        # brute force solution
-        # k = 1
-        # while True:
-        #     total_time = 0
-        #     for pile in piles:
-        #         total_time += math.ceil(pile / k)
-        #         if total_time > h:
-        #             break
-
-        #     if total_time <= h:
-        #         return k
-        #     k += 1
+        return min_speed
