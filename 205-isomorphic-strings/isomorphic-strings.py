b8cba0d34efb323160c5f7791a96527b1f17087f
Ankur <ankur.kumar.dev@outlook.com>
2025-09-23 16:00:24 +0530

    Time: 7 ms (56.23%) | Memory: 18 MB (36.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,28 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # cleaner version
-        s2t, t2s = {}, {}
-        for a, b in zip(s, t):
-            if s2t.get(a, b) != b or t2s.get(b,a) != a:
+        s2t = {}
+        t2s = {}
+
+        for i,j in zip(s, t):
+            if s2t.get(i, j) != j or t2s.get(j, i) != i:
                 return False
-            s2t[a] = b
-            t2s[b] = a
+            s2t[i] = j
+            t2s[j] = i
+        
         return True
-        
-        
-        
-        # Working solution, optimal
-        # s_to_t = {}
-        # t_to_s = {}
-        
-        # for i in range(len(s)):
-        #     if s[i] not in s_to_t and t[i] not in t_to_s:
-        #         s_to_t[s[i]] = t[i]
-        #         t_to_s[t[i]] = s[i]
-        #     elif s[i] in s_to_t and t[i] in t_to_s:
-        #         if s_to_t[s[i]] != t[i] or t_to_s[t[i]] != s[i]:
-        #             return False
-        #     else:
-        #         return False
-        
-        # return True
