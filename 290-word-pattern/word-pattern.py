6ec05f7db35bc5075e45c41dd4bdee51e6d6f740
Ankur <ankur.kumar.dev@outlook.com>
2025-09-23 16:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.91%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,16 +1,24 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        # clean solution
         s_list = s.split(" ")
-        
+        output = []
         if len(pattern) != len(s_list):
             return False
 
         pattern_map = {}
         s_map = {}
         for char,word in zip(pattern, s_list):
+            # if pattern_map.get(char, word) != word or s_map.get(word, char) != char:
+                # return False
+
             if pattern_map.get(char, word) != word or s_map.get(word, char) != char:
+                print("no word pattern")
                 return False
             pattern_map[char] = word
             s_map[word] = char
+
+
         
+        print(s)
         return True
